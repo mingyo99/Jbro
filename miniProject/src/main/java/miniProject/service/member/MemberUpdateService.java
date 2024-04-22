e8d603997f562e980a8d3d5a50b02948ac83805f
HKEDU <HKEDU@DESKTOP-8F2RU57>
2024-04-22 17:42:57 +0900

    프로필 이미지 추가

--- miniProject/src/main/java/miniProject/service/member/MemberUpdateService.java
+++ miniProject/src/main/java/miniProject/service/member/MemberUpdateService.java
@@ -1,7 +1,12 @@
 package miniProject.service.member;
 
+import java.io.File;
+import java.net.URL;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import miniProject.command.MemberCommand;
 import miniProject.domain.MemberDTO;
@@ -24,6 +29,25 @@ public class MemberUpdateService {
 		dto.setMemberPhone2(memberCommand.getMemberPhone2());
 		dto.setMemberPost(memberCommand.getMemberPost());
 		dto.setWeight(memberCommand.getWeight());
+		
+		URL resource = getClass().getClassLoader().getResource("static/upload");
+		System.out.println(resource);
+		String filrDir = "C:\\dev\\src5\\miniProject\\target\\classes\\static\\upload";
+		MultipartFile mf = memberCommand.getProfileImg();
+		String originalFile = mf.getOriginalFilename();
+		String extension = originalFile.substring(originalFile.lastIndexOf("."));
+		String storeName = UUID.randomUUID().toString().replace("-", "");
+		String storeFileName = storeName + extension;
+		File file = new File(filrDir+"/"+storeFileName);
+		try {
+			mf.transferTo(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		dto.setProfileImg(storeFileName);
+		dto.setProfileImgImg(originalFile);
+		
 		memberMapper.memberUpdate(dto);
 	}
 }
